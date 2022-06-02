34343e2c786499f7a7abdb8227537eb580ac46b5
droganov <serge@droganov.ru>
2022-06-02 12:40:25 +0200

    Create documentation for theme palette
    * start fill colors table
    
    * table of colors paper adde to palette.stories.tsx
    
    * Add stroke colors
    
    * adjust palette for dark mode
    
    Co-authored-by: AleksandrRazhev <alexandrrazh@gmail.com>

--- src/react/colors/palette.stories.tsx
+++ src/react/colors/palette.stories.tsx
@@ -1,74 +1,171 @@
 import { ComponentStory } from '@storybook/react'
+import clsx from 'clsx'
 
 export default {
   title: 'Colors/Palette'
 }
 
-const Template: ComponentStory<() => JSX.Element> = () => (
-  <div className="flex gap-2">
-    <div className="flex flex-col gap-2">
-      <div className="text-ink-primary border-ink-primary dark:text-ink-primary-dark dark:border-ink-primary-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
-        T
-      </div>
-      <div className="ui-bg-primary text-ink rounded-full w-12 h-12 flex items-center justify-center">
-        T
-      </div>
-    </div>
-    <div className="flex flex-col gap-2 mr-14">
-      <div className="text-ink-secondary border-ink-secondary dark:text-ink-secondary-dark dark:border-ink-secondary-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
-        T
-      </div>
-      <div className="ui-bg-secondary text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
-        T
-      </div>
-    </div>
-    <div className="flex flex-col gap-2">
-      <div className="text-ink-error border-ink-error dark:text-ink-error-dark dark:border-ink-error-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
-        T
-      </div>
-      <div className="ui-bg-error text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
-        T
-      </div>
-    </div>
-    <div className="flex flex-col gap-2">
-      <div className="text-ink-success border-ink-success dark:text-ink-success-dark dark:border-ink-success-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
-        T
-      </div>
-      <div className="ui-bg-success text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
-        T
-      </div>
-    </div>
-    <div className="flex flex-col gap-2">
-      <div className="text-ink-warning border-ink-warning dark:text-ink-warning-dark dark:border-ink-warning-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
-        T
-      </div>
-      <div className="ui-bg-warning text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
-        T
-      </div>
-    </div>
-    <div className="flex flex-col gap-2 mr-14">
-      <div className="text-ink-info border-ink-info dark:text-ink-info-dark dark:border-ink-info-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
-        T
-      </div>
-      <div className="ui-bg-info text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
-        T
-      </div>
-    </div>
+const Fill = ({
+  dark,
+  value
+}: {
+  dark?: boolean
+  value: string
+}): JSX.Element => (
+  <div className="flex items-center gap-4">
+    <div
+      className={clsx(
+        'w-7 h-7 rounded-full border',
+        value,
+        dark ? 'border-ink-border-dark' : 'border-ink-border'
+      )}
+    />
+    {value}
+  </div>
+)
 
-    <div className="flex flex-col gap-2">
-      <div className="text-ink border-ink dark:text-ink-dark dark:border-ink-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
-        T
+const Stroke = ({ value }: { value: string }): JSX.Element => (
+  <div className="flex items-center gap-4">
+    <div className={clsx('w-7 h-7 rounded border-2', value)} />
+    {value}
+  </div>
+)
+
+const ExtraFill = ({ value }: { value: string }): JSX.Element => (
+  <div className="flex items-center gap-4">
+    <div className={clsx('w-7 h-7 rounded', value)} />
+    {value}
+  </div>
+)
+
+const Template: ComponentStory<() => JSX.Element> = () => (
+  <div className="dark:text-ink-dark">
+    <h2 className="text-4xl mb-4 ml-4">Fill Colors</h2>
+    <div className="sm:grid grid-cols-2 gap-2 mb-12">
+      <div>
+        <h4 className="font-bold ml-4">Normal</h4>
+        <div className="p-4 grid gap-2 bg-paper text-ink rounded">
+          <Fill value="bg-paper" />
+          <Fill value="bg-paper-2" />
+          <Fill value="bg-paper-3" />
+          <Fill value="bg-paper-primary" />
+          <Fill value="bg-paper-secondary" />
+          <Fill value="bg-paper-error" />
+          <Fill value="bg-paper-success" />
+          <Fill value="bg-paper-warning" />
+          <Fill value="bg-paper-info" />
+        </div>
       </div>
-      <div className="ui-bg-paper text-ink dark:text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
-        T
+      <div>
+        <h4 className="font-bold ml-4">Dark</h4>
+        <div className="p-4 bg-paper-dark text-ink-dark rounded grid gap-2">
+          <Fill dark value="bg-paper-dark" />
+          <Fill dark value="bg-paper-2-dark" />
+          <Fill value="bg-paper-3-dark" />
+          <Fill value="bg-paper-primary-dark" />
+          <Fill value="bg-paper-secondary-dark" />
+          <Fill value="bg-paper-error-dark" />
+          <Fill value="bg-paper-success-dark" />
+          <Fill value="bg-paper-warning-dark" />
+          <Fill value="bg-paper-info-dark" />
+        </div>
       </div>
     </div>
-    <div className="flex flex-col gap-2 justify-end">
-      <div className="text-ink border-ink-border dark:border-ink-border-dark dark:text-ink-dark dark:border-ink-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
-        T
+    <h2 className="text-4xl mb-4 ml-4">Stroke Colors</h2>
+    <div className="sm:grid grid-cols-2 gap-2 mb-12">
+      <div>
+        <h4 className="font-bold ml-4">Normal</h4>
+        <div className="p-4 grid gap-2 bg-paper text-ink rounded">
+          <Stroke value="border-ink" />
+          <Stroke value="border-ink-2" />
+          <Stroke value="border-ink-primary" />
+          <Stroke value="border-ink-secondary" />
+          <Stroke value="border-ink-error" />
+          <Stroke value="border-ink-success" />
+          <Stroke value="border-ink-warning" />
+          <Stroke value="border-ink-info" />
+          <Stroke value="border-ink-border" />
+        </div>
       </div>
-      <div className="ui-bg-paper-2 text-ink dark:text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
-        T
+      <div>
+        <h4 className="font-bold ml-4">Dark</h4>
+        <div className="p-4 bg-paper-dark text-ink-dark rounded grid gap-2">
+          <Stroke value="border-ink-dark" />
+          <Stroke value="border-ink-2-dark" />
+          <Stroke value="border-ink-primary-dark" />
+          <Stroke value="border-ink-secondary-dark" />
+          <Stroke value="border-ink-error-dark" />
+          <Stroke value="border-ink-success-dark" />
+          <Stroke value="border-ink-warning-dark" />
+          <Stroke value="border-ink-info-dark" />
+          <Stroke value="border-ink-border-dark" />
+        </div>
+      </div>
+    </div>
+    <h2 className="text-4xl mb-4 ml-4">Link Colors</h2>
+    <div className="sm:grid grid-cols-2 gap-2 mb-12">
+      <div>
+        <h4 className="font-bold ml-4">Normal</h4>
+        <div className="p-4 grid gap-2 bg-paper text-ink rounded">
+          <span className="text-link">text-link</span>
+          <span className="text-link-hover">text-link-hover</span>
+          <span className="text-link-active">text-link-active</span>
+          <span className="text-link-visited">text-link-visited</span>
+        </div>
+      </div>
+      <div>
+        <h4 className="font-bold ml-4">Dark</h4>
+        <div className="p-4 bg-paper-dark text-ink-dark rounded grid gap-2">
+          <span className="text-link-dark">text-link-dark</span>
+          <span className="text-link-hover-dark">text-link-hover-dark</span>
+          <span className="text-link-active-dark">text-link-active-dark</span>
+          <span className="text-link-visited-dark">text-link-visited-dark</span>
+        </div>
+      </div>
+    </div>
+    <h2 className="text-4xl mb-4 ml-4">Extra Fill Colors</h2>
+    <div className="sm:grid grid-cols-2 gap-2 mb-12">
+      <div>
+        <h4 className="font-bold ml-4">Normal</h4>
+        <div className="p-4 grid gap-2 bg-paper text-ink rounded">
+          <ExtraFill value="bg-1" />
+          <ExtraFill value="bg-2" />
+          <ExtraFill value="bg-3" />
+          <ExtraFill value="bg-4" />
+          <ExtraFill value="bg-5" />
+          <ExtraFill value="bg-6" />
+          <ExtraFill value="bg-7" />
+          <ExtraFill value="bg-8" />
+          <ExtraFill value="bg-9" />
+          <ExtraFill value="bg-10" />
+          <ExtraFill value="bg-11" />
+          <ExtraFill value="bg-12" />
+          <ExtraFill value="bg-13" />
+          <ExtraFill value="bg-14" />
+          <ExtraFill value="bg-15" />
+          <ExtraFill value="bg-16" />
+        </div>
+      </div>
+      <div>
+        <h4 className="font-bold ml-4">Dark</h4>
+        <div className="p-4 bg-paper-dark text-ink-dark rounded grid gap-2">
+          <ExtraFill value="bg-1-dark" />
+          <ExtraFill value="bg-2-dark" />
+          <ExtraFill value="bg-3-dark" />
+          <ExtraFill value="bg-4-dark" />
+          <ExtraFill value="bg-5-dark" />
+          <ExtraFill value="bg-6-dark" />
+          <ExtraFill value="bg-7-dark" />
+          <ExtraFill value="bg-8-dark" />
+          <ExtraFill value="bg-9-dark" />
+          <ExtraFill value="bg-10-dark" />
+          <ExtraFill value="bg-11-dark" />
+          <ExtraFill value="bg-12-dark" />
+          <ExtraFill value="bg-13-dark" />
+          <ExtraFill value="bg-14-dark" />
+          <ExtraFill value="bg-15-dark" />
+          <ExtraFill value="bg-16-dark" />
+        </div>
       </div>
     </div>
   </div>

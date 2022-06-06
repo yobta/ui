f6bdf44e8e65fc5459831b7a7706dd4b7acff125
droganov <serge@droganov.ru>
2022-06-06 11:19:19 +0200

    clean vtest config up (#11)

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,14 +3,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // esbuild: {
-  //   target: 'node14',
-  //   format: 'cjs'
-  // },
   test: {
-    // deps: {
-    //   interopDefault: false
-    // },
     globals: true,
     environment: 'happy-dom'
   }

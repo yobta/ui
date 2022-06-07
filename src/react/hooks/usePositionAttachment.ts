a9b25c31cd5a2237c950bf9b008698ef3a274c1a
AleksandrRazhev <alexandrrazh@gmail.com>
2022-06-07 16:57:26 +0300

    switch/case added to usePositionAttachment.ts

--- src/react/hooks/usePositionAttachment.ts
+++ src/react/hooks/usePositionAttachment.ts
@@ -1,14 +1,14 @@
 interface PositionAttachmentHook {
   (args: {
     align:
-      | 'top'
-      | 'top-left'
-      | 'top-right'
-      | 'bottom'
-      | 'bottom-left'
-      | 'bottom-right'
-      | 'right'
-      | 'left'
+    | 'top'
+    | 'top-left'
+    | 'top-right'
+    | 'bottom'
+    | 'bottom-left'
+    | 'bottom-right'
+    | 'right'
+    | 'left'
     // | 'auto'
     consumerNode: HTMLElement | null
     producerNode: HTMLElement | null
@@ -17,14 +17,14 @@ interface PositionAttachmentHook {
     x: number
     y: number
     align:
-      | 'top'
-      | 'top-left'
-      | 'top-right'
-      | 'bottom'
-      | 'bottom-left'
-      | 'bottom-right'
-      | 'right'
-      | 'left'
+    | 'top'
+    | 'top-left'
+    | 'top-right'
+    | 'bottom'
+    | 'bottom-left'
+    | 'bottom-right'
+    | 'right'
+    | 'left'
   } | null
 }
 
@@ -41,13 +41,54 @@ export const usePositionAttachment: PositionAttachmentHook = ({
   let finalAlign = align
 
   switch (finalAlign) {
+    case 'top':
+      return {
+        align: finalAlign,
+        x: rect.x + rect.width / 2,
+        y: rect.y - offset
+      }
+    case 'top-left':
+      return {
+        align: finalAlign,
+        x: rect.x,
+        y: rect.y - offset
+      }
+    case 'top-right':
+      return {
+        align: finalAlign,
+        x: rect.x + rect.width,
+        y: rect.y - offset
+      }
     case 'bottom':
       return {
         align: finalAlign,
         x: rect.x + rect.width / 2,
         y: rect.y + rect.height + offset
       }
-    case 'top':
+    case 'bottom-left':
+      return {
+        align: finalAlign,
+        x: rect.x,
+        y: rect.y + rect.height + offset
+      }
+    case 'bottom-right':
+      return {
+        align: finalAlign,
+        x: rect.x + rect.width,
+        y: rect.y + rect.height + offset
+      }
+    case 'left':
+      return {
+        align: finalAlign,
+        x: rect.x - offset,
+        y: rect.y + rect.height / 2
+      }
+    case 'right':
+      return {
+        align: finalAlign,
+        x: rect.x + rect.width + offset,
+        y: rect.y + rect.height / 2
+      }
     default:
       return {
         align: 'top',

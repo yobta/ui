a256127b8b9057dc8767be7ca141ddc4dd0189cc
AleksandrRazhev <alexandrrazh@gmail.com>
2022-06-07 17:52:03 +0300

    left-top, left-bottom, right-top, right-bottom added to usePositionAttachment

--- src/react/hooks/usePositionAttachment.ts
+++ src/react/hooks/usePositionAttachment.ts
@@ -8,7 +8,11 @@ interface PositionAttachmentHook {
     | 'bottom-left'
     | 'bottom-right'
     | 'right'
+    | 'right-top'
+    | 'right-bottom'
     | 'left'
+    | 'left-top'
+    | 'left-bottom'
     // | 'auto'
     consumerNode: HTMLElement | null
     producerNode: HTMLElement | null
@@ -24,7 +28,11 @@ interface PositionAttachmentHook {
     | 'bottom-left'
     | 'bottom-right'
     | 'right'
+    | 'right-top'
+    | 'right-bottom'
     | 'left'
+    | 'left-top'
+    | 'left-bottom'
   } | null
 }
 
@@ -83,11 +91,35 @@ export const usePositionAttachment: PositionAttachmentHook = ({
         x: rect.x - offset,
         y: rect.y + rect.height / 2
       }
+    case 'left-top':
+      return {
+        align: finalAlign,
+        x: rect.x - offset,
+        y: rect.y
+      }
+    case 'left-bottom':
+      return {
+        align: finalAlign,
+        x: rect.x - offset,
+        y: rect.y + rect.height
+      }
     case 'right':
       return {
         align: finalAlign,
         x: rect.x + rect.width + offset,
         y: rect.y + rect.height / 2
+      }
+    case 'right-top':
+      return {
+        align: finalAlign,
+        x: rect.x + rect.width + offset,
+        y: rect.y
+      }
+    case 'right-bottom':
+      return {
+        align: finalAlign,
+        x: rect.x + rect.width + offset,
+        y: rect.y + rect.height
       }
     default:
       return {

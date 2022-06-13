64bad87b0afbb81abbd8f56ed788965b8238ebf9
AleksandrRazhev <alexandrrazh@gmail.com>
2022-06-13 17:51:26 +0300

    tooltip style fixed

--- src/react/Tooltip/getTooltipStyle.ts
+++ src/react/Tooltip/getTooltipStyle.ts
@@ -14,25 +14,25 @@ export const getTooltipStyle: GetTooltipStyle = (position, offset) => {
       return {
         top: y,
         left: x,
-        transform: `translate(-50%, ${offset}px)`
+        transform: `translate(-50%, calc(-100% - ${offset}px))`
       }
     case 'top-left':
       return {
         top: y,
         left: x,
-        transform: `translate(0, ${offset}px)`
+        transform: `translate(0, calc(-100% - ${offset}px))`
       }
     case 'top-right':
       return {
         top: y,
         left: x,
-        transform: `translate(-100%, ${offset}px)`
+        transform: `translate(-100%, calc(-100% - ${offset}px))`
       }
     case 'right':
       return {
         top: y,
         left: x,
-        transform: `translate(${offset}px, 0)`
+        transform: `translate(${offset}px, -50%)`
       }
     case 'right-top':
       return {
@@ -44,25 +44,25 @@ export const getTooltipStyle: GetTooltipStyle = (position, offset) => {
       return {
         top: y,
         left: x,
-        transform: `translate(${offset}px, 0)`
+        transform: `translate(${offset}px, -100%)`
       }
     case 'left':
       return {
         top: y,
         left: x,
-        transform: `translate(${offset}px, 0)`
+        transform: `translate(calc(-100% - ${offset}px), -50%)`
       }
     case 'left-top':
       return {
         top: y,
         left: x,
-        transform: `translate(${offset}px, 0)`
+        transform: `translate(calc(-100% - ${offset}px), 0)`
       }
     case 'left-bottom':
       return {
         top: y,
         left: x,
-        transform: `translate(${offset}px, 0)`
+        transform: `translate(calc(-100% - ${offset}px), -100%)`
       }
     case 'bottom-left':
       return {

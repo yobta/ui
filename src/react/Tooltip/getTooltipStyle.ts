8069ff5b10e14e71edd235c2ba3a56a903423110
AleksandrRazhev <alexandrrazh@gmail.com>
2022-06-13 16:21:02 +0300

    getTooltipStyle changed

--- src/react/Tooltip/getTooltipStyle.ts
+++ src/react/Tooltip/getTooltipStyle.ts
@@ -11,8 +11,59 @@ export const getTooltipStyle: GetTooltipStyle = (position, offset) => {
   let { align, x, y } = position
   switch (align) {
     case 'top':
-      break
-
+      return {
+        top: y,
+        left: x,
+        transform: `translate(-50%, ${offset}px)`
+      }
+    case 'top-left':
+      return {
+        top: y,
+        left: x,
+        transform: `translate(0, ${offset}px)`
+      }
+    case 'top-right':
+      return {
+        top: y,
+        left: x,
+        transform: `translate(-100%, ${offset}px)`
+      }
+    case 'right':
+      return {
+        top: y,
+        left: x,
+        transform: `translate(${offset}px, 0)`
+      }
+    case 'right-top':
+      return {
+        top: y,
+        left: x,
+        transform: `translate(${offset}px, 0)`
+      }
+    case 'right-bottom':
+      return {
+        top: y,
+        left: x,
+        transform: `translate(${offset}px, 0)`
+      }
+    case 'left':
+      return {
+        top: y,
+        left: x,
+        transform: `translate(${offset}px, 0)`
+      }
+    case 'left-top':
+      return {
+        top: y,
+        left: x,
+        transform: `translate(${offset}px, 0)`
+      }
+    case 'left-bottom':
+      return {
+        top: y,
+        left: x,
+        transform: `translate(${offset}px, 0)`
+      }
     case 'bottom-left':
       return {
         top: y,

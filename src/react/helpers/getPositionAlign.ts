03f62fef2b142cc8b67242831ec8c930e7d00d69
AleksandrRazhev <alexandrrazh@gmail.com>
2022-06-16 19:08:28 +0300

    getPositionAlign changed

--- src/react/helpers/getPositionAlign.ts
+++ src/react/helpers/getPositionAlign.ts
@@ -16,83 +16,72 @@ interface AutoAlign {
   (producerNode: HTMLElement, consumerNode: HTMLElement, offset: number, align: AlignOptions | undefined): AlignOptions
 }
 
+interface Sizes {
+  width: number,
+  height: number
+}
+
+interface Positions {
+  top: number,
+  left: number,
+  right: number,
+  bottom: number
+}
+
+interface PossiblePositions {
+  top: boolean,
+  right: boolean,
+  bottom: boolean,
+  left: boolean
+}
+
 export const getPositionAlign: AutoAlign = (producerNode, consumerNode, offset, align) => {
-  let windowWidth: number = document.documentElement.clientWidth
-  let windowHeigth: number = document.documentElement.clientHeight
-  console.log(`Window Width: ${windowWidth}, Window height: ${windowHeigth}`)
-  let rectProducer: DOMRect = producerNode.getBoundingClientRect()
-  let rectConsumer: DOMRect = consumerNode.getBoundingClientRect()
 
-  interface Sizes {
-    width: number,
-    height: number
+  let positionCalc = (): { possiblePosition: PossiblePositions } => {
+
+    let windowWidth: number = document.documentElement.clientWidth
+    let windowHeigth: number = document.documentElement.clientHeight
+    let rectProducer: DOMRect = producerNode.getBoundingClientRect()
+    let rectConsumer: DOMRect = consumerNode.getBoundingClientRect()
+    let sizesConsumer: Sizes = {
+      width: rectConsumer.width,
+      height: rectConsumer.height
+    }
+    let positions: Positions = {
+      top: rectProducer.top,
+      right: windowWidth - rectProducer.left - rectProducer.width,
+      bottom: windowHeigth - rectProducer.top - rectProducer.height,
+      left: rectProducer.left
+    }
+    let comparePositions: Positions = {
+      top: positions.top - sizesConsumer.height - 2 * offset,
+      right: positions.right - sizesConsumer.width - 2 * offset,
+      bottom: positions.bottom - sizesConsumer.height - 2 * offset,
+      left: positions.left - sizesConsumer.width - 2 * offset
+    }
+    let possiblePosition: PossiblePositions = {
+      top: comparePositions.top > 0,
+      right: (comparePositions.right > 0 && (positions.top > 0 && positions.bottom > 0)),
+      bottom: comparePositions.bottom > 0,
+      left: (comparePositions.left > 0 && (positions.top > 0 && positions.bottom > 0))
+    }
+    return { possiblePosition }
   }
 
-  interface Positions {
-    top: number,
-    left: number,
-    right: number,
-    bottom: number
-  }
-
-  let sizesProducer: Sizes = {
-    width: rectProducer.width,
-    height: rectProducer.height
-  }
-  console.log(sizesProducer)
-
-  let sizesConsumer: Sizes = {
-    width: rectConsumer.width,
-    height: rectConsumer.height
-  }
-  console.log(sizesConsumer)
-
-  let positions: Positions = {
-    top: rectProducer.top,
-    right: windowWidth - rectProducer.left - rectProducer.width,
-    bottom: windowHeigth - rectProducer.top - rectProducer.height,
-    left: rectProducer.left
-  }
-  console.log(positions)
-
-  let comparePositions: Positions = {
-    top: positions.top - sizesConsumer.height - 2 * offset,
-    right: positions.right - sizesConsumer.width - 2 * offset,
-    bottom: positions.bottom - sizesConsumer.height - 2 * offset,
-    left: positions.left - sizesConsumer.width - 2 * offset
-  }
-  console.log(comparePositions)
-
-  interface PossiblePositions {
-    top: boolean,
-    right: boolean,
-    bottom: boolean,
-    left: boolean
-  }
-
-  let possiblePosition: PossiblePositions = {
-    top: comparePositions.top > 0,
-    right: (comparePositions.right > 0 && (positions.top > 0 && positions.bottom > 0)),
-    bottom: comparePositions.bottom > 0,
-    left: (comparePositions.left > 0 && (positions.top > 0 && positions.bottom > 0))
-  }
-  console.log(possiblePosition)
-
-  let finalPosition = (): string | undefined => {
+  let finalPosition = (): string => {
     if (!align) align = 'top'
 
-    let value: number = comparePositions[align.split('-')[0]]
-    console.log(`value: ${value}`)
+    let position: string = align.split('-')[0]
+    let positions: [string, boolean][] = Object.entries(positionCalc().possiblePosition)
 
-    if (value > 0) return align
-
-    let autoPosition: string = Object.entries(comparePositions).reduce((acc, item) => {
-      if (item[1] > acc[1]) return item
-      return acc
-    })[0]
-    console.log(autoPosition)
-
-    return autoPosition
+    let check: [string, boolean] | undefined = positions.find((item: [string, boolean]) => {
+      if (item[0] === position && item[1]) return true
+    })
+    if (check) return align
+    let find: [string, boolean] | undefined = positions.find((item: [string, boolean]) => item[1])
+    if (find[0]) {
+      return find[0]
+    } else {return 'top'}
   }
 
   switch (finalPosition()) {
@@ -110,6 +99,4 @@ export const getPositionAlign: AutoAlign = (producerNode, consumerNode, offset, 
     case 'right-bottom': return 'right-bottom'
     default: return 'top'
   }
-
-  // return 'bottom'
 }

f037e049654ea01f67895d48b769ae3d3a2e42af
AleksandrRazhev <alexandrrazh@gmail.com>
2022-06-21 14:24:01 +0300

    renderHook added

--- src/react/hooks/usePopoverCoordinates/usePopoverCoordinates.spec.ts
+++ src/react/hooks/usePopoverCoordinates/usePopoverCoordinates.spec.ts
@@ -1,14 +1,34 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { renderHook } from '@testing-library/react'
 
 import { usePopoverCoordinates } from './usePopoverCoordinates.js'
 
-test('consumerNode = null, producerNode = null', () => {
-  let result = usePopoverCoordinates({
-    placement: 'top',
-    preferredPlacement: 'top',
-    consumerNode: null,
-    producerNode: null,
-    offset: 8
+describe('usePopoverCoordinates Hook', () => {
+  describe('consumerNode = null, producerNode = null', () => {
+    it("placement = 'top', preferredPlacement = 'top'", () => {
+      let { result } = renderHook(() => {
+        usePopoverCoordinates({
+          placement: 'top',
+          preferredPlacement: 'top',
+          consumerNode: null,
+          producerNode: null,
+          offset: 8
+        })
+      })
+      expect(result).toEqual({ undefined })
+    })
+    it('placement = undefined, preferredPlacement = undefined', () => {
+      let { result } = renderHook(() => {
+        usePopoverCoordinates({
+          placement: undefined,
+          preferredPlacement: undefined,
+          consumerNode: null,
+          producerNode: null,
+          offset: 8
+        })
+      })
+      expect(result).toEqual({ undefined })
+    })
   })
-  expect(result).toEqual('top')
+
 })

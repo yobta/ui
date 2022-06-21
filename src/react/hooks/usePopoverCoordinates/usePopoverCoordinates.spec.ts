4b60c014e17d11e50c2e01d66fa8a4fc84d49f21
AleksandrRazhev <alexandrrazh@gmail.com>
2022-06-21 15:07:30 +0300

    HTMLElement added

--- src/react/hooks/usePopoverCoordinates/usePopoverCoordinates.spec.ts
+++ src/react/hooks/usePopoverCoordinates/usePopoverCoordinates.spec.ts
@@ -4,31 +4,59 @@ import { renderHook } from '@testing-library/react'
 import { usePopoverCoordinates } from './usePopoverCoordinates.js'
 
 describe('usePopoverCoordinates Hook', () => {
-  describe('consumerNode = null, producerNode = null', () => {
-    it("placement = 'top', preferredPlacement = 'top'", () => {
-      let { result } = renderHook(() => {
-        usePopoverCoordinates({
-          placement: 'top',
-          preferredPlacement: 'top',
-          consumerNode: null,
-          producerNode: null,
-          offset: 8
-        })
+  it('is undefined when consumerNode and producerNode are null', () => {
+    let { result } = renderHook(() => {
+      usePopoverCoordinates({
+        placement: 'top',
+        preferredPlacement: 'top',
+        consumerNode: null,
+        producerNode: null,
+        offset: 8
       })
-      expect(result).toEqual({ undefined })
     })
-    it('placement = undefined, preferredPlacement = undefined', () => {
-      let { result } = renderHook(() => {
-        usePopoverCoordinates({
-          placement: undefined,
-          preferredPlacement: undefined,
-          consumerNode: null,
-          producerNode: null,
-          offset: 8
-        })
-      })
-      expect(result).toEqual({ undefined })
-    })
+    expect(result).toEqual({ undefined })
   })
+  it('is undefined when placement and preferredPlacement are undefined', () => {
+    let { result } = renderHook(() => {
+      usePopoverCoordinates({
+        placement: undefined,
+        preferredPlacement: undefined,
+        consumerNode: null,
+        producerNode: null,
+        offset: 8
+      })
+    })
+    expect(result).toEqual({ undefined })
+  })
+  it('is when consumerNode and producerNode are defined', () => {
+    let consumerNode = {
+      getBoundingClientRect: () => ({
+        top: 100,
+        bottom: 100,
+        height: 100,
+        width: 100
+      })
+    } as HTMLElement
 
+    let producerNode = {
+      getBoundingClientRect: () => ({
+        top: 100,
+        bottom: 100,
+        height: 100,
+        width: 100
+      })
+    } as HTMLElement
+
+    let { result } = renderHook(() => {
+      usePopoverCoordinates({
+        placement: 'right',
+        preferredPlacement: 'left',
+        consumerNode,
+        producerNode,
+        offset: 8
+      })
+    })
+
+    expect(result).toEqual({ undefined })
+  })
 })

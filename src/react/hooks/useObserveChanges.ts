e9c6a33a2a47d4b024f107e69be9e4ad9898129f
droganov <serge@droganov.ru>
2022-06-23 14:32:50 +0200

    revert ObserveChangesHook

--- src/react/hooks/useObserveChanges.ts
+++ src/react/hooks/useObserveChanges.ts
@@ -11,8 +11,6 @@ export const useObserveChanges: ObserveChangesHook = ({
   let [state, setState] = useState(0)
 
   useEffect(() => {
-    let isActive = producerNode && !disabled
-    let resizeObzerver: ResizeObserver | undefined
     let update = (): void => {
       setState(lastState => lastState + 1)
     }
@@ -21,13 +19,8 @@ export const useObserveChanges: ObserveChangesHook = ({
       ? new ResizeObserver(update)
       : null
 
-    if (isActive) {
-      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-      if (window.ResizeObserver) {
-        resizeObzerver = new ResizeObserver(update)
-        // @ts-ignore
-        resizeObzerver.observe(producerNode)
-      }
+    if (producerNode && !disabled) {
+      resizeObzerver?.observe(producerNode)
       window.addEventListener('scroll', update, { passive: true })
       window.addEventListener('resize', update, { passive: true })
     }

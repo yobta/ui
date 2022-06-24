fbb310b2d60c1c78de8e822f59c00b620482f2f6
droganov <serge@droganov.ru>
2022-06-24 13:08:24 +0200

    rename ui-divider

--- src/tailwind-preset/dividerPlugin.cjs
+++ src/tailwind-preset/dividerPlugin.cjs
@@ -2,7 +2,7 @@ const plugin = require('tailwindcss/plugin')
 
 module.exports = plugin(({ addBase }) => {
   addBase({
-    'hr, .ui-divider': {
+    'hr, .yobta-divider': {
       borderWidth: '1px 0 0 0',
       borderTopColor: 'currentColor',
       opacity: 0.32

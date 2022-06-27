e674bb87ff902d9f265fb3cce0500a32501e7e6e
droganov <serge@droganov.ru>
2022-06-27 10:57:56 +0200

    make linter happy

--- src/react/helpers/whenOnline/whenOnline.spec.ts
+++ src/react/helpers/whenOnline/whenOnline.spec.ts
@@ -1,20 +1,20 @@
 /* eslint-disable @typescript-eslint/no-confusing-void-expression */
 import { it, expect, vi } from 'vitest'
 
-import { whenOnline } from '../whenOnline'
+import { whenOnline } from './whenOnline.js'
 
 it('should be eventually true when online', async () => {
   vi.stubGlobal('navigator', { onLine: true })
-  const result = await whenOnline()
+  let result = await whenOnline()
   expect(result).toBe(undefined)
 })
 
 it('shuld wait for online event when pffline', async () => {
-  const addEventListenerMock = vi.fn()
+  let addEventListenerMock = vi.fn()
   vi.stubGlobal('navigator', { onLine: false })
   vi.stubGlobal('addEventListener', addEventListenerMock)
 
-  const promise = whenOnline()
+  let promise = whenOnline()
   expect(addEventListenerMock).toHaveBeenCalledWith(
     'online',
     expect.any(Function)

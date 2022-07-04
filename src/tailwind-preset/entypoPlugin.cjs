a5bb262451bb8cd4a535f8899f9ce61432c5abd9
droganov <serge@droganov.ru>
2022-07-04 13:10:24 +0200

    Icons docs

--- src/tailwind-preset/entypoPlugin.cjs
+++ src/tailwind-preset/entypoPlugin.cjs
@@ -5,7 +5,8 @@ const applyPrefixed = require('./applyPrefixed.cjs')
 module.exports = plugin(({ addBase, prefix }) => {
   addBase({
     '.yobta-entypo': {
-      ...applyPrefixed(prefix, '.fill-current', '.h-5', '.w-5')
+      ...applyPrefixed(prefix, '.fill-current', '.h-5', '.w-5'),
+      margin: '2px'
     }
   })
 })

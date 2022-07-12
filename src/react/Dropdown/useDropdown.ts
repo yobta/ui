ef970e7e38ad116f9fa422088052e88a3417f798
droganov <serge@droganov.ru>
2022-07-12 11:09:10 +0200

    separate getAnimationClassName

--- src/react/Dropdown/useDropdown.ts
+++ src/react/Dropdown/useDropdown.ts
@@ -15,6 +15,7 @@ import {
   usePortalNode
 } from '../hooks/index.js'
 import { PopoverPlacementOptions } from '../hooks/usePopoverCoordinates/getOptimalPopoverPlacement.js'
+import { getAnimationClassName } from './getAnimationClassName.js'
 import { useDropdownStyle } from './useDropdownStyle.js'
 
 interface DropDownHook {
@@ -36,37 +37,6 @@ interface DropDownHook {
 }
 
 const hiddenClassName = 'yobta-dropdown-menu--hidden'
-
-const getAnimationClassName = (
-  placement: PopoverPlacementOptions,
-  visible: boolean
-): string => {
-  switch (placement) {
-    case 'top':
-    case 'top-left':
-    case 'top-right':
-      return visible
-        ? 'animate-yobta-dropdown-in-top'
-        : 'animate-yobta-dropdown-out-top'
-    case 'right':
-    case 'right-top':
-    case 'right-bottom':
-      return visible
-        ? 'animate-yobta-dropdown-in-right'
-        : 'animate-yobta-dropdown-out-right'
-    case 'left':
-    case 'left-top':
-    case 'left-bottom':
-      return visible
-        ? 'animate-yobta-dropdown-in-left'
-        : 'animate-yobta-dropdown-out-left'
-
-    default:
-      return visible
-        ? 'animate-yobta-dropdown-in-bottom'
-        : 'animate-yobta-dropdown-out-bottom'
-  }
-}
 
 export const useDropdown: DropDownHook = ({
   blockLevel,

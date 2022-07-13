83029184fc668d1547d3f8d179634f9cb88bf1cd
AleksandrRazhev <alexandrrazh@gmail.com>
2022-07-13 18:29:31 +0300

    colors in test changed

--- src/tailwind-preset/selectedPlugin.spec.ts
+++ src/tailwind-preset/selectedPlugin.spec.ts
@@ -9,9 +9,33 @@ test('selectedPlugin', async () => {
   let addSelected = vi.fn()
   let colors = {
     selected: {
-      2: {
-        paper: { DEFAULT: 'selected2', dark: 'selectedDark2' },
-        ink: { DEFAULT: 'selectedText2', dark: 'selectedDarkText2' }
+      '2': {
+        paper: {
+          DEFAULT: 'var(--yobta-color-selected-2, #AED5FA)',
+          dark: 'var(--yobta-color-selected-2-dark, #5B81A5)'
+        },
+        ink: {
+          DEFAULT: 'var(--yobta-color-selected-text, #414141)',
+          dark: 'var(--yobta-color-selected-text-dark, #FCFCFC)'
+        }
+      },
+      '3': {
+        paper: {
+          DEFAULT: 'var(--yobta-color-selected-3, #A8C2DC)',
+          dark: 'var(--yobta-color-selected-3-dark, #4B7194)'
+        },
+        ink: {
+          DEFAULT: 'var(--yobta-color-selected-text, #414141)',
+          dark: 'var(--yobta-color-selected-text-dark, #FCFCFC)'
+        }
+      },
+      'DEFAULT': {
+        paper: 'var(--yobta-color-selected, #CBE6FF)',
+        ink: 'var(--yobta-color-selected-text, #414141)'
+      },
+      'dark': {
+        paper: 'var(--yobta-color-selected-dark, #4B6278)',
+        ink: 'var(--yobta-color-selected-text-dark, #FCFCFC)'
       }
     }
   }
@@ -22,6 +46,18 @@ test('selectedPlugin', async () => {
   expect(addSelected).toBeCalledWith({
     '.yobta-selected-2': {
       '@apply yobta-selected-2-paper yobta-selected-2-ink dark:yobta-selected-2-paper-dark dark:yobta-selected-2-ink-dark':
+        {}
+    },
+    '.yobta-selected-3': {
+      '@apply yobta-selected-3-paper yobta-selected-3-ink dark:yobta-selected-3-paper-dark dark:yobta-selected-3-ink-dark':
+        {}
+    },
+    '.yobta-selected-DEFAULT': {
+      '@apply yobta-selected-DEFAULT-paper yobta-selected-DEFAULT-ink dark:yobta-selected-DEFAULT-paper-dark dark:yobta-selected-DEFAULT-ink-dark':
+        {}
+    },
+    '.yobta-selected-dark': {
+      '@apply yobta-selected-dark-paper yobta-selected-dark-ink dark:yobta-selected-dark-paper-dark dark:yobta-selected-dark-ink-dark':
         {}
     }
   })

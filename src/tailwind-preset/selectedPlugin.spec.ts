7d89cfa634b43ada600ff8b37f8c921faeedae28
AleksandrRazhev <alexandrrazh@gmail.com>
2022-07-13 22:44:11 +0300

    select plugin test changed

--- src/tailwind-preset/selectedPlugin.spec.ts
+++ src/tailwind-preset/selectedPlugin.spec.ts
@@ -6,58 +6,38 @@ import selectedPlugin from './selectedPlugin.cjs'
 const prefix = (str: string): string => str
 
 test('selectedPlugin', async () => {
-  let addSelected = vi.fn()
+  let addUtilities = vi.fn()
   let colors = {
     selected: {
-      '2': {
-        paper: {
-          DEFAULT: 'var(--yobta-color-selected-2, #AED5FA)',
-          dark: 'var(--yobta-color-selected-2-dark, #5B81A5)'
-        },
-        ink: {
-          DEFAULT: 'var(--yobta-color-selected-text, #414141)',
-          dark: 'var(--yobta-color-selected-text-dark, #FCFCFC)'
-        }
+      1: {
+        paper: { DEFAULT: 'selected', dark: 'selectedDark' },
+        ink: { DEFAULT: 'selectedText', dark: 'selectedDarkText' }
       },
-      '3': {
-        paper: {
-          DEFAULT: 'var(--yobta-color-selected-3, #A8C2DC)',
-          dark: 'var(--yobta-color-selected-3-dark, #4B7194)'
-        },
-        ink: {
-          DEFAULT: 'var(--yobta-color-selected-text, #414141)',
-          dark: 'var(--yobta-color-selected-text-dark, #FCFCFC)'
-        }
+      2: {
+        paper: { DEFAULT: 'selected2', dark: 'selectedDark2' },
+        ink: { DEFAULT: 'selectedText2', dark: 'selectedDarkText2' }
       },
-      'DEFAULT': {
-        paper: 'var(--yobta-color-selected, #CBE6FF)',
-        ink: 'var(--yobta-color-selected-text, #414141)'
-      },
-      'dark': {
-        paper: 'var(--yobta-color-selected-dark, #4B6278)',
-        ink: 'var(--yobta-color-selected-text-dark, #FCFCFC)'
+      3: {
+        paper: { DEFAULT: 'selected3', dark: 'selectedDark3' },
+        ink: { DEFAULT: 'selectedText3', dark: 'selectedDarkText3' }
       }
     }
   }
   let theme = (): typeof colors => colors
 
-  selectedPlugin.handler({ addSelected, prefix, theme })
+  selectedPlugin.handler({ addUtilities, prefix, theme })
 
-  expect(addSelected).toBeCalledWith({
+  expect(addUtilities).toBeCalledWith({
+    '.yobta-selected': {
+      '@apply bg-selected-1-paper dark:bg-selected-1-paper-dark text-selected-1-ink dark:text-selected-1-ink-dark':
+        {}
+    },
     '.yobta-selected-2': {
-      '@apply yobta-selected-2-paper yobta-selected-2-ink dark:yobta-selected-2-paper-dark dark:yobta-selected-2-ink-dark':
+      '@apply bg-selected-2-paper dark:bg-selected-2-paper-dark text-selected-2-ink dark:text-selected-2-ink-dark':
         {}
     },
     '.yobta-selected-3': {
-      '@apply yobta-selected-3-paper yobta-selected-3-ink dark:yobta-selected-3-paper-dark dark:yobta-selected-3-ink-dark':
-        {}
-    },
-    '.yobta-selected-DEFAULT': {
-      '@apply yobta-selected-DEFAULT-paper yobta-selected-DEFAULT-ink dark:yobta-selected-DEFAULT-paper-dark dark:yobta-selected-DEFAULT-ink-dark':
-        {}
-    },
-    '.yobta-selected-dark': {
-      '@apply yobta-selected-dark-paper yobta-selected-dark-ink dark:yobta-selected-dark-paper-dark dark:yobta-selected-dark-ink-dark':
+      '@apply bg-selected-3-paper dark:bg-selected-3-paper-dark text-selected-3-ink dark:text-selected-3-ink-dark':
         {}
     }
   })

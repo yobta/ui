38a96472fba4b1c3a87f60756e1f70463081fa54
AleksandrRazhev <alexandrrazh@gmail.com>
2022-07-14 18:53:31 +0300

    divider plugin spec added

---
+++ src/tailwind-preset/dividerPlugin.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect, vi } from 'vitest'
+
+// @ts-ignore
+import dividerPlugin from './dividerPlugin.cjs'
+
+test('dividerPlugin', async () => {
+  let addBase = vi.fn()
+
+  dividerPlugin.handler({ addBase })
+  expect(addBase).toBeCalledWith({
+    'hr, .yobta-divider': {
+      borderWidth: '1px 0 0 0',
+      borderTopColor: 'currentColor',
+      opacity: 0.32
+    }
+  })
+})

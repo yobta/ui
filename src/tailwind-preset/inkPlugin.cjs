57e514b64436bea7e517d2b10495a410205f7cc0
AlexandrRazhev <alexandrrazh@gmail.com>
2022-07-20 20:59:22 +0300

    check inkColors by ink plugin added

--- src/tailwind-preset/inkPlugin.cjs
+++ src/tailwind-preset/inkPlugin.cjs
@@ -21,6 +21,24 @@ module.exports = plugin(({ addUtilities, prefix, theme }) => {
     throw new Error('Yobta inkPlugin: theme.colors.ink.dark should be a string')
   }
 
+  Object.entries(inkColors).forEach(item => {
+    // for (let key in item[1]) {
+    Object.entries(item[1]).forEach(key => {
+      console.log(key)
+      if (key[0] !== 'DEFAULT' && key[0] !== 'dark') {
+        throw new Error(
+          `Yobta inkPlugin: theme.colors.ink.${item[0]} have invalid field '${key[0]}'`
+        )
+      }
+      if (typeof key[1] !== 'string') {
+        console.log(key[1])
+        throw new Error(
+          `Yobta inkPlugin: theme.colors.ink.${key[0]}: ${key[1]} should be a string`
+        )
+      }
+    })
+  })
+
   if (DEFAULT && dark) {
     addUtilities({
       '.yobta-ink': {

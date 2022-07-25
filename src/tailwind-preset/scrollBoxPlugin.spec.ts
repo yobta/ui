c583133b58cfbad535340819b44171a2bafd8b86
AleksandrRazhev <alexandrrazh@gmail.com>
2022-07-25 15:45:20 +0300

    scroll box plugin spec added

---
+++ src/tailwind-preset/scrollBoxPlugin.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect, vi } from 'vitest'
+
+// @ts-ignore
+import scrollBoxPlugin from './scrollBoxPlugin.cjs'
+
+const prefix = (str: string): string => str
+
+test('scrollBoxPlugin', async () => {
+  let addUtilities = vi.fn()
+
+  scrollBoxPlugin.handler({ addUtilities, prefix })
+  expect(addUtilities).toBeCalledWith({
+    '.yobta-scroll-box': {
+      'overflow': 'hidden',
+      'scroll-behavior': 'smooth',
+      'scrollbar-width': 'none',
+      '-ms-overflow-style': 'none',
+      '-webkit-overflow-scrolling': 'touch',
+      '&::-webkit-scrollbar': {
+        display: 'none'
+      }
+    },
+    '.yobta-scroll-box-x': {
+      '@apply yobta-scroll-box': {},
+      'display': 'flex',
+      'justifyContent': 'flex-start',
+      'overflow-x': 'scroll',
+      '&::-webkit-scrollbar': {
+        display: 'none'
+      }
+    },
+    '.yobta-scroll-box-y': {
+      '@apply yobta-scroll-box': {},
+      'overflow-y': 'scroll',
+      '&::-webkit-scrollbar': {
+        display: 'none'
+      }
+    }
+  })
+})

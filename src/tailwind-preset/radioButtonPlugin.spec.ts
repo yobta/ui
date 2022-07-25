5f50f360fa919a5818814281a10113051c432244
AleksandrRazhev <alexandrrazh@gmail.com>
2022-07-25 14:04:59 +0300

    radio button plugin spec added

---
+++ src/tailwind-preset/radioButtonPlugin.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect, vi } from 'vitest'
+
+// @ts-ignore
+import radioButtonPlugin from './radioButtonPlugin.cjs'
+
+const prefix = (str: string): string => str
+
+test('radioButtonPlugin', async () => {
+  let addBase = vi.fn()
+
+  radioButtonPlugin.handler({ addBase, prefix })
+  expect(addBase).toBeCalledWith({
+    '.yobta-radio': {
+      '@apply appearance-none border-2 border-current rounded-full cursor-pointer relative':
+        {},
+      'height': '1.5rem',
+      'width': '1.5rem',
+      'minWidth': '1.5rem',
+
+      '&::before': {
+        '@apply absolute bg-current left-1 top-1 right-1 bottom-1 transform scale-0 transition duration-100':
+          {},
+        'borderRadius': 'inherit',
+        'content': '""'
+      },
+      '&:checked::before': {
+        '@apply scale-100': {}
+      },
+      '&:invalid': {
+        '@apply bg-ink-error dark:bg-ink-error-dark text-ink-error dark:text-ink-error-dark':
+          {}
+      }
+    }
+  })
+})

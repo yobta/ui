6e0a763a6e27596d02878fc4acfd872c8cd82712
droganov <serge@droganov.ru>
2022-07-28 12:20:26 +0200

    Add controllable state to the placeholder

--- src/react/Placeholder/Placeholder.tsx
+++ src/react/Placeholder/Placeholder.tsx
@@ -2,11 +2,19 @@ import clsx from 'clsx'
 import { ReactNode } from 'react'
 
 interface PlaceholderFC {
-  (props: { children?: ReactNode; className?: string }): JSX.Element
+  (props: {
+    children?: ReactNode
+    className?: string
+    visible?: boolean
+  }): JSX.Element
 }
 
-export const Placeholder: PlaceholderFC = ({ children, className }) => {
-  if (typeof children === 'undefined') {
+export const Placeholder: PlaceholderFC = ({
+  children,
+  className,
+  visible
+}) => {
+  if (typeof children === 'undefined' || visible) {
     return <span className={clsx('yobta-placeholder', className)} />
   }
   return children as unknown as JSX.Element

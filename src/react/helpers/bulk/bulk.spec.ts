5b528cebc00425800cb5d2017053bf75035ecac6
AleksandrRazhev <alexandrrazh@gmail.com>
2022-08-09 14:25:01 +0300

    beCalled to equal changed

--- src/react/helpers/bulk/bulk.spec.ts
+++ src/react/helpers/bulk/bulk.spec.ts
@@ -5,8 +5,5 @@ import { bulk } from './bulk.js'
 test('bulk', () => {
   let func: VoidFunction = (): void => {}
   let args: VoidFunction[] = [func]
-  bulk(...args)
-  expect(() => {
-    bulk(...args)
-  }).toBeCalled()
+  expect(bulk(...args)).toEqual(undefined)
 })

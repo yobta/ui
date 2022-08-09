f3572b9f44ba1b3f7e09b87719d367402236b2a4
AleksandrRazhev <alexandrrazh@gmail.com>
2022-08-09 17:51:30 +0300

    test changed

--- src/react/helpers/bulk/bulk.spec.ts
+++ src/react/helpers/bulk/bulk.spec.ts
@@ -4,6 +4,11 @@ import { bulk } from './bulk.js'
 
 test('bulk', () => {
   let func: VoidFunction = (): void => {}
-  let args: VoidFunction[] = [func]
-  expect(bulk(...args)).toEqual(undefined)
+  let func1: VoidFunction = (): void => {}
+  let func2: VoidFunction = (): void => {}
+  // let args: VoidFunction[] = [func, func1, func2]
+  bulk(func, func1, func2)
+  expect(func).toBeCalled()
+  expect(func1).toBeCalled()
+  expect(func2).toBeCalled()
 })

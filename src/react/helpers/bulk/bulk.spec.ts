a35490765bacfb55035a343062a6ab7c2c8c0e44
AleksandrRazhev <alexandrrazh@gmail.com>
2022-08-10 15:34:39 +0300

    call order check added

--- src/react/helpers/bulk/bulk.spec.ts
+++ src/react/helpers/bulk/bulk.spec.ts
@@ -3,11 +3,13 @@ import { test, expect, vi } from 'vitest'
 import { bulk } from './bulk.js'
 
 test('calls the arguments sequentially', () => {
-  let func = vi.fn()
-  let func1 = vi.fn()
-  let func2 = vi.fn()
+  let arr: string[] = []
+  let func = vi.fn(() => arr.push('func'))
+  let func1 = vi.fn(() => arr.push('func1'))
+  let func2 = vi.fn(() => arr.push('func2'))
   bulk(func, func1, func2)
   expect(func).toBeCalled()
   expect(func1).toBeCalled()
   expect(func2).toBeCalled()
+  expect(arr).toEqual(['func', 'func1', 'func2'])
 })

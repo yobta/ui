6fed7459ba90b33da4cb514d91793ae0fbace6ab
Pavel Brylev <pavel.brylev@supremedesign.ru>
2022-08-11 15:33:08 +0300

    fix: rest background

--- .storybook/DocsContainer.jsx
+++ .storybook/DocsContainer.jsx
@@ -3,29 +3,44 @@ import { DocsContainer as BaseContainer } from '@storybook/addon-docs/blocks'
 import { useDarkMode } from 'storybook-dark-mode'
 import { themes } from '@storybook/theming'
 
+const styles = /* CSS */ `
+  .yobta-paper.improve > .sbdocs.sbdocs-wrapper {
+    background: inherit;
+  }
+
+  .yobta-paper.improve .sbdocs.sbdocs-preview {
+    background: inherit;
+  }
+`
+
 export const DocsContainer = ({ children, context }) => {
   const dark = useDarkMode()
 
   return (
-    <BaseContainer
-      context={{
-        ...context,
-        storyById: id => {
-          const storyContext = context.storyById(id)
-          return {
-            ...storyContext,
-            parameters: {
-              ...storyContext?.parameters,
-              docs: {
-                ...storyContext?.parameters?.docs,
-                theme: dark ? themes.dark : themes.light
+    <>
+      <style>{styles}</style>
+      <div className="yobta-paper improve">
+        <BaseContainer
+          context={{
+            ...context,
+            storyById: id => {
+              const storyContext = context.storyById(id)
+              return {
+                ...storyContext,
+                parameters: {
+                  ...storyContext?.parameters,
+                  docs: {
+                    ...storyContext?.parameters?.docs,
+                    theme: dark ? themes.dark : themes.light
+                  }
+                }
               }
             }
-          }
-        }
-      }}
-    >
-      {children}
-    </BaseContainer>
+          }}
+        >
+          {children}
+        </BaseContainer>
+      </div>
+    </>
   )
 }

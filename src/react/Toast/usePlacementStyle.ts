4e3eb7def4af67f4f0df2cfa167cec1492f77f60
droganov <serge@droganov.ru>
2022-08-12 13:56:11 +0200

    fix max-width issue

--- src/react/Toast/usePlacementStyle.ts
+++ src/react/Toast/usePlacementStyle.ts
@@ -7,27 +7,33 @@ interface PalcementStyleHook {
     | {
         top: number
         left: string
+        maxWidth: string
         transform: string
       }
     | {
         top: number
         left: number
+        maxWidth: string
       }
     | {
+        maxWidth: string
         top: number
         right: number
       }
     | {
         bottom: number
         left: number
+        maxWidth: string
       }
     | {
         bottom: number
+        maxWidth: string
         right: number
       }
     | {
         bottom: number
         left: string
+        maxWidth: string
         transform: string
       }
 }
@@ -36,31 +42,37 @@ type PalcementStyle = ReturnType<PalcementStyleHook>
 
 export const usePlacementStyle: PalcementStyleHook = (placement, offset) =>
   useMemo((): PalcementStyle => {
+    let maxWidth = `calc(100vw - ${offset * 2}px)`
     switch (placement) {
       case 'top':
         return {
           top: 0 + offset,
           left: '50%',
+          maxWidth,
           transform: 'translateX(-50%)'
         }
       case 'top-left':
         return {
           top: 0 + offset,
-          left: 0 + offset
+          left: 0 + offset,
+          maxWidth
         }
       case 'top-right':
         return {
+          maxWidth,
           top: 0 + offset,
           right: 0 + offset
         }
       case 'bottom-left':
         return {
           bottom: 0 + offset,
-          left: 0 + offset
+          left: 0 + offset,
+          maxWidth
         }
       case 'bottom-right':
         return {
           bottom: 0 + offset,
+          maxWidth,
           right: 0 + offset
         }
 
@@ -69,6 +81,7 @@ export const usePlacementStyle: PalcementStyleHook = (placement, offset) =>
         return {
           bottom: 0 + offset,
           left: '50%',
+          maxWidth,
           transform: 'translateX(-50%)'
         }
     }

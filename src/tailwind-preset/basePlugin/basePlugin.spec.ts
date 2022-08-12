1d04b32bbcae01cf5522de083b5657eee38e5427
AleksandrRazhev <alexandrrazh@gmail.com>
2022-08-12 18:22:24 +0300

    spec fixed

--- src/tailwind-preset/basePlugin/basePlugin.spec.ts
+++ src/tailwind-preset/basePlugin/basePlugin.spec.ts
@@ -12,6 +12,9 @@ test('basePlugin', () => {
   expect(addBase).toBeCalledWith({
     '.yobta-disabled': {
       '@apply pointer-events-none opacity-50': {}
+    },
+    '.yobta-no-tap': {
+      '@apply rgb(0, 0, 0, 0)': {}
     }
   })
 })

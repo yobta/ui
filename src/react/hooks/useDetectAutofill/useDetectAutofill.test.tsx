5cca0b5cfcb9285491a877d85561748ba324f142
Pavel Brylev <pavel.brylev@supremedesign.ru>
2022-08-12 16:43:12 +0300

    improve: extend tests

--- src/react/hooks/useDetectAutofill/useDetectAutofill.test.tsx
+++ src/react/hooks/useDetectAutofill/useDetectAutofill.test.tsx
@@ -1,9 +1,15 @@
 import { it, expect, beforeEach, vi } from 'vitest'
-import { renderHook, render, act } from '@testing-library/react'
+import {
+  renderHook,
+  render,
+  act,
+  fireEvent,
+  screen
+} from '@testing-library/react'
 // @ts-expect-error
 import React, { MutableRefObject, useRef } from 'react'
 
-import { useDetectAutofill } from './useDetectAutofill.js'
+import { useDetectAutofill, isAutofilled } from './useDetectAutofill.js'
 
 type HookProps = Parameters<typeof useDetectAutofill>[0]
 
@@ -17,17 +23,54 @@ beforeEach(async () => {
   render(<input data-testid="target" ref={inputRef} />)
 })
 
+it('enough only one selector in isAutofilled', async () => {
+  let matchesMock = vi
+    .spyOn<HTMLInputElement, 'matches'>(inputRef.current!, 'matches')
+    .mockImplementation(matcher => matcher === ':-webkit-autofill')
+
+  let result = isAutofilled(inputRef.current, [
+    ':-internal-autofill-selected',
+    ':-webkit-autofill',
+    ':autofill'
+  ])
+
+  expect(matchesMock).toBeCalled()
+  expect(result).toBe(true)
+})
+
+it('return false when matcher throw error in isAutofilled', async () => {
+  let matchesMock = vi
+    .spyOn<HTMLInputElement, 'matches'>(inputRef.current!, 'matches')
+    .mockImplementation(() => {
+      throw new Error('Test error')
+    })
+
+  let result = isAutofilled(inputRef.current, [':-webkit-autofill'])
+
+  expect(matchesMock).toBeCalledWith(':-webkit-autofill')
+  expect(result).toBe(false)
+})
+
+it('return false unless node in isAutofilled', async () => {
+  let result = isAutofilled(null, [':-webkit-autofill'])
+
+  expect(result).toBe(false)
+})
+
 it('is false when not matсhes', async () => {
   let { result } = renderHook<boolean, HookProps>(useDetectAutofill, {
     initialProps: inputRef
   })
 
-  Object.assign(inputRef.current as object, { matches: () => false })
+  let matchesMock = vi
+    .spyOn<HTMLInputElement, 'matches'>(inputRef.current!, 'matches')
+    .mockImplementation(() => false)
 
   expect(result.current).toBe(false)
 
   await new Promise(resolve => setTimeout(resolve, 3000))
 
+  expect(matchesMock).toBeCalled()
   expect(result.current).toBe(false)
 })
 
@@ -37,9 +80,13 @@ it('returns true when matches', async () => {
   })
   expect(result.current).toBe(false)
 
+  let matchesMock = vi
+    .spyOn<HTMLInputElement, 'matches'>(inputRef.current!, 'matches')
+    .mockImplementation(() => false)
+
   await act(async () => {
     await new Promise(resolve => setTimeout(resolve, 100))
-    Object.assign(inputRef.current as object, { matches: () => true })
+    matchesMock.mockImplementation(() => true)
     await new Promise(resolve => setTimeout(resolve, 500))
   })
 
@@ -52,11 +99,38 @@ it('resets after timeout', async () => {
   })
   expect(result.current).toBe(false)
 
+  let matchesMock = vi
+    .spyOn<HTMLInputElement, 'matches'>(inputRef.current!, 'matches')
+    .mockImplementation(() => false)
+
   await act(async () => {
     await new Promise(resolve => setTimeout(resolve, 3000))
-    Object.assign(inputRef.current as object, { matches: () => true })
+    matchesMock.mockImplementation(() => true)
     await new Promise(resolve => setTimeout(resolve, 500))
   })
 
+  expect(matchesMock).toBeCalled()
+  expect(result.current).toBe(false)
+})
+
+it('resets on input', async () => {
+  let { result } = renderHook<boolean, HookProps>(useDetectAutofill, {
+    initialProps: inputRef
+  })
+  expect(result.current).toBe(false)
+
+  let matchesMock = vi
+    .spyOn<HTMLInputElement, 'matches'>(inputRef.current!, 'matches')
+    .mockImplementation(() => false)
+
+  await act(async () => {
+    let inputField = await screen.findByTestId('target')
+    fireEvent.input(inputField)
+    await new Promise(resolve => setTimeout(resolve, 200))
+    matchesMock.mockImplementation(() => true)
+    await new Promise(resolve => setTimeout(resolve, 200))
+  })
+
+  expect(matchesMock).not.toBeCalled()
   expect(result.current).toBe(false)
 })

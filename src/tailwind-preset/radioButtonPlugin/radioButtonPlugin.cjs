17b673bfe3ddf584ce1d8ddd8ff33f2316b1d936
AlexandrRazhev <alexandrrazh@gmail.com>
2022-08-15 13:50:31 +0300

    no-tap added to radioButton

--- src/tailwind-preset/radioButtonPlugin/radioButtonPlugin.cjs
+++ src/tailwind-preset/radioButtonPlugin/radioButtonPlugin.cjs
@@ -7,6 +7,7 @@ module.exports = plugin(({ addBase, prefix }) => {
     '.yobta-radio': {
       ...applyPrefixed(
         prefix,
+        '.yobta-no-tap',
         '.appearance-none',
         '.border-2',
         '.border-current',

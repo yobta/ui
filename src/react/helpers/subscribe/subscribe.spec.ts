b874b9cdd76777f66ac86ee524fb7bae2936260c
AleksandrRazhev <alexandrrazh@gmail.com>
2022-09-02 18:30:08 +0300

    tect edited

--- src/react/helpers/subscribe/subscribe.spec.ts
+++ src/react/helpers/subscribe/subscribe.spec.ts
@@ -1,12 +1,21 @@
-import { test, expect, vi } from 'vitest'
+import { it, expect, vi } from 'vitest'
 
 import { subscribe } from './subscribe.js'
 
-test('subscribe', () => {
+it('onLine is true', () => {
   let target = window
   let eventType = 'online'
   let callBack = vi.fn()
 
   subscribe(target, eventType, callBack)
   expect(target.navigator.onLine).toEqual(true)
+})
+
+it('document', () => {
+  let target = document
+  let eventType = 'DOMContentLoaded'
+  let callBack = vi.fn()
+
+  subscribe(target, eventType, callBack)
+  expect(document)
 })

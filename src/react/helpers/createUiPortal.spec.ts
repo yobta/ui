6543dfe38f35d6238a852d9029583dda93e95e17
AleksandrRazhev <alexandrrazh@gmail.com>
2022-09-08 21:00:37 +0300

    react-dom created

--- src/react/helpers/createUiPortal.spec.ts
+++ src/react/helpers/createUiPortal.spec.ts
@@ -1,7 +1,21 @@
 import { it, expect } from 'vitest'
+import { screen } from '@testing-library/react'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import { createUiPortal } from './createUiPortal.tsx'
+import { createUiPortal } from './createUiPortal.js'
 
 it('createUiPortal', () => {
-  expect(createUiPortal)
+  let root = document.createElement('div')
+  root.id = 'root'
+  document.body.append(root)
+
+  ReactDOM.render(
+    React.createElement('div', { id: 'nodeElem' }, 'Element'),
+    document.getElementById('root')
+  )
+
+  let element = screen.getByText('Element')
+
+  expect(element)
 })

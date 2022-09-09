edddf209fbdabf25e190c8c1797b333fb178995d
AleksandrRazhev <alexandrrazh@gmail.com>
2022-09-09 14:05:22 +0300

    snapshot added

--- src/react/helpers/createUiPortal.spec.tsx
+++ src/react/helpers/createUiPortal.spec.tsx
@@ -1,29 +1,22 @@
 import { it, expect } from 'vitest'
-import { screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import React from 'react'
-import { createRoot } from 'react-dom/client'
 
 import { createUiPortal } from './createUiPortal.js'
 
 it('createUiPortal', () => {
-  let rootElement = document.createElement('div')
-  rootElement.id = 'root'
-  document.body.append(rootElement)
-
   let App = (): JSX.Element => {
     return (
       <>
-        {createUiPortal(<span>NewText</span>, 'nodeid')}
+        {createUiPortal(<span>NewElement</span>, 'nodeid')}
         <div id="nodeid">Element</div>
       </>
     )
   }
 
-  let container = document.getElementById('root')
-  let root = createRoot(container!)
-  root.render(<App />)
+  render(<App />)
+  let element = screen.getByText<HTMLElement>('Element')
 
-  let element = screen.getByText('NewText')
-
-  expect(element)
+  screen.debug()
+  expect(element).toMatchSnapshot()
 })

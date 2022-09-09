a84a11ed7c85df010f06df9554e55b7f28e019a1
AleksandrRazhev <alexandrrazh@gmail.com>
2022-09-09 17:10:52 +0300

    portal component created

--- src/react/helpers/createUiPortal.spec.tsx
+++ src/react/helpers/createUiPortal.spec.tsx
@@ -6,16 +6,15 @@ import { createUiPortal } from './createUiPortal.js'
 
 it('creates a component and passes the node from the portal to it', () => {
   let App = (): JSX.Element => {
-    return (
-      <>
-        {createUiPortal(<span>NewElement</span>, 'nodeid')}
-        <div id="nodeid">Element</div>
-      </>
-    )
+    return React.createElement('div', { id: 'nodeid' }, 'Element')
+  }
+
+  let Portal = (): JSX.Element => {
+    return <>{createUiPortal(<span>NewElement</span>, 'nodeid')}</>
   }
 
   render(<App />)
+  render(<Portal />)
   let element = screen.getByText<HTMLElement>('Element')
-
   expect(element).toMatchSnapshot()
 })
